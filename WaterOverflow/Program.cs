7fa63b30dce821d908fce9b5c72f4d8c14c17061
saif <saify@DESKTOP-44MOAKM>
2024-05-13 15:09:29 +0200

    Organisera koden

--- WaterOverflow/Program.cs
+++ WaterOverflow/Program.cs
@@ -1,11 +1,15 @@
-﻿class WaterOverflow
+﻿using System;
+using WaterOverflow;
+
+class Program
 {
 	static void Main()
 	{
 		int row = ReadInput("Rad");
 		int glass = ReadInput("Glas");
 
-		double timeToOverflow = CalculateTimeToOverflow(row, glass);
+		Pyramid pyramid = new Pyramid(row);
+		double timeToOverflow = pyramid.CalculateTimeToOverflow(glass);
 		PrintResult(timeToOverflow);
 
 		Console.ReadLine(); // För att hålla konsolen öppen
@@ -15,21 +19,6 @@
 	{
 		Console.Write($"{prompt} ? ");
 		return int.Parse(Console.ReadLine()!);
-	}
-
-	static double CalculateTimeToOverflow(int row, int glass)
-	{
-		// Beräkna det totala antalet glas i pyramidens nedersta rad
-		int totalGlasses = (row * (row + 1)) / 2;
-
-		// Beräkna vattenmängden i det angivna glaset när det börjar rinna över
-		double waterInGlass = 10.0 * (glass - 1);
-		double levelInGlass = waterInGlass + 5.0 * (row - 1); // Totalt vatten i glaset
-
-		// Beräkna tidpunkten när glaset börjar rinna över
-		double timeToOverflow = (levelInGlass - 2.6667) / 2.0 * 10.0; // Formel för tid
-
-		return timeToOverflow;
 	}
 
 	static void PrintResult(double timeToOverflow)
---
+++ WaterOverflow/Pyramid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterOverflow
+{
+	public class Pyramid
+	{
+		private int rows;
+
+		public Pyramid(int rows)
+		{
+			this.rows = rows;
+		}
+
+		public double CalculateTimeToOverflow(int glass)
+		{
+			int totalGlasses = (rows * (rows + 1)) / 2;
+			double waterInGlass = 10.0 * (glass - 1);
+			double levelInGlass = waterInGlass + 5.0 * (rows - 1);
+			return (levelInGlass - 2.6667) / 2.0 * 10.0;
+		}
+	}
+}
